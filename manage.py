7c3ba2249c67f97fdfb1a30594ec708363c9572f
Rahulprasada <rahulprasadkpm@gmail.com>
2024-12-10 16:05:09 +0530

    manage.py changes

--- manage.py
+++ manage.py
@@ -2,11 +2,11 @@ import os
 from django.core.wsgi import get_wsgi_application
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproj.settings')
 
 # This checks if the app is running in the Vercel environment
 if os.environ.get('VERCEL'):
-    from . import app  # Import your app to be used as the handler
-    handler = app  # Set 'app' as the handler for Vercel
+    from . import myapp  # Import your app to be used as the handler
+    handler = myapp  # Set 'app' as the handler for Vercel
 else:
     application = get_wsgi_application()  # For local or other environments, use the default WSGI application
--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path # type: ignore
 from . import views  # Import views from your app
 
 urlpatterns = [
-    path('', views.login, name='one'),  # Maps the root URL to the 'home' view
+    path('', views.login, name='home'),  # Maps the root URL to the 'home' view
     path('about/', views.signup, name='about'),  # Maps '/about' to the 'about' view
 ]
